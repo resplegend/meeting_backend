e9797a159deb3f0ae708a656a59c231b1fde6d75
resplegend <milanzubic1120@gmail.com>
2025-07-03 08:00:10 +0200

    fix crypto module issue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
