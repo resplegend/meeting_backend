a0e8545d052aae84f2498c9954a1372fe9c2c465
resplegend <milanzubic1120@gmail.com>
2025-07-03 08:10:05 +0200

    update crypto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as crypto from 'crypto';
-(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
